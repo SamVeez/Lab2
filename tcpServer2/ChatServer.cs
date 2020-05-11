db2b1340da63e02e5412d705154dc71ac60f1f27
SamVeez <49760035+SamVeez@users.noreply.github.com>
2020-05-11 12:34:44 +0300

    Add files via upload

---
+++ tcpServer2/ChatServer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace tcpServer2
+{
+    public class ChatServer
+    {
+        private static ServerObject server;
+        private static Thread listenThread;
+        private static void Main()
+        {
+            try
+            {
+                server = new ServerObject();
+                listenThread = new Thread(server.Listen);
+                listenThread.Start();
+            }
+            catch (Exception ex)
+            {
+                server.Disconnect();
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ tcpServer2/ClientObject.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace tcpServer2
+{
+    public class ClientObject
+    {
+        protected internal string Id { get; }
+        protected internal NetworkStream Stream { get; private set; }
+        private string userName;
+        private readonly TcpClient client;
+        private readonly ServerObject server;
+
+        public ClientObject(TcpClient tcpClient, ServerObject serverObject)
+        {
+            Id = Guid.NewGuid().ToString();
+            client = tcpClient;
+            server = serverObject;
+            serverObject.AddConnection(this);
+        }
+        public void Process()
+        {
+            try
+            {
+                Stream = client.GetStream();
+                var message = GetMessage();
+                userName = message;
+
+                message = userName + " вошел в чат";
+                server.BroadcastMessage(message, Id);
+                Console.WriteLine(message);
+                while (true)
+                {
+                    try
+                    {
+                        message = GetMessage();
+                        message = $"{userName}: {message}";
+                        Console.WriteLine(message);
+                        server.BroadcastMessage(message, this.Id);
+                    }
+                    catch
+                    {
+                        message = $"{userName}: покинул чат";
+                        Console.WriteLine(message);
+                        server.BroadcastMessage(message, this.Id);
+                        break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                server.RemoveConnection(Id);
+                Close();
+            }
+        }
+        private string GetMessage()
+        {
+            var data = new byte[64];
+            var builder = new StringBuilder();
+            do
+            {
+                var bytes = Stream.Read(data, 0, data.Length);
+                builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+            }
+            while (Stream.DataAvailable);
+
+            return builder.ToString();
+        }
+        protected internal void Close()
+        {
+            Stream?.Close();
+            client?.Close();
+        }
+    }
+}
